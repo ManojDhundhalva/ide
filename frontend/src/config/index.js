a250660ce1a24767fa0881f4fd0d5c5433753063
ManojDhundhalva <manojdhundhalva60@gmail.com>
2025-12-01 17:42:49 +0530

    changes

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
     // BACKEND_API: import.meta.env.VITE_BACKEND_API,
     // GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-    BACKEND_API: "https://ec2-44-221-45-164.compute-1.amazonaws.com",
+    BACKEND_API: "http://ec2-44-221-45-164.compute-1.amazonaws.com",
     GOOGLE_CLIENT_ID: "64493502854-9ejfpv00ciclpifns0mor80fk7gmlngt.apps.googleusercontent.com",
 };
 
